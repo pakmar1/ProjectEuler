3b592aa10b54f2458dae734a7180b7bcb48086df
Prasad Akmar <31735926+pakmar1@users.noreply.github.com>
2018-12-12 12:40:47 -0500

    Add files via upload

---
+++ determineSum.py
@@ -0,0 +1,14 @@
+def determineMultiples():
+    
+    sum1=0
+    for a in range(1000):
+        if a % 3 == 0 or a % 5 == 0:
+            sum1+=a
+    
+    print(sum1)
+
+def main():
+    determineMultiples()
+
+if __name__ == "__main__":
+    main()
